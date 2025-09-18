b21308291627677870880f5a2299e25e063b02ad
judasane <judasane@gmail.com>
2025-09-18 13:43:45 +0000

    feat(env): Configure Project IDX environment
    Adds the `.idx/dev.nix` file to configure the development environment for Project IDX.
    
    This configuration specifies the required Node.js version, adds the Angular IDE extension for a better developer experience, and sets up the correct command for running the web preview.

---
+++ .idx/dev.nix
@@ -0,0 +1,26 @@
+@dev.nix{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "angular.ng-template"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
